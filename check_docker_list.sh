ffade7d50420596582dcafa23cca5598761417d3
stebr1 <patrick.zambelli@wuerth-phoenix.com>
2020-11-30 15:13:58 +0100

    script docker list

---
+++ check_docker_list.sh
@@ -0,0 +1,47 @@
+declare -r output='0b9aa6a92e31        simplifierag/appserver:v5_5_126_129   "/opt/simplifier/binΓÇª"   6 days ago          Up 6 days           8080/tcp, 8090/tcp
+              simplifier' 
+#Parsing Arguments (Array)
+declare -a id=($@)
+
+#Declare Status
+declare -i ok_status='0'
+declare -i bad_status='1'
+
+#Declare Exit Code
+declare -i ok_exit='0'
+declare -i bad_exit='2'
+
+#Declare Array
+declare -a found
+declare -a not_found
+
+
+#Confirm Arguments
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 2
+fi
+
+#Iterate over array and execute command
+for x in ${id[@]}  
+    do
+    echo $output | grep $x > /dev/null 2>&1
+
+    export status=$? 
+    
+    if [ $status -eq $ok_status ]; then
+        found+=$x' is running! ' 
+    elif [ $status -eq $bad_status ]; then
+        not_found+=$x' is not running! '
+    fi
+done
+
+
+#Exit Status
+if [ ${#not_found[@]} -gt 0 ]; then
+    echo 'CRITICAL: '$not_found'''(OK: '$found')'
+    exit $bad_exit
+else
+    echo 'OK: '$found''
+    exit $ok_exit
+fi
---
+++ check_docker_list_v2.sh
@@ -0,0 +1,46 @@
+declare -r output=docker container ls
+#Parsing Arguments (Array)
+declare -a id=($@)
+
+#Declare Status
+declare -i ok_status='0'
+declare -i bad_status='1'
+
+#Declare Exit Code
+declare -i ok_exit='0'
+declare -i bad_exit='2'
+
+#Declare Array
+declare -a found
+declare -a not_found
+
+
+#Confirm Arguments
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 2
+fi
+
+#Iterate over array and execute command
+for x in ${id[@]}  
+    do
+    echo $output | grep $x > /dev/null 2>&1
+
+    export status=$? 
+    
+    if [ $status -eq $ok_status ]; then
+        found+=$x' is running! ' 
+    elif [ $status -eq $bad_status ]; then
+        not_found+=$x' is not running! '
+    fi
+done
+
+
+#Exit Status
+if [ ${#not_found[@]} -gt 0 ]; then
+    echo 'CRITICAL: '$not_found'''(OK: '$found')'
+    exit $bad_exit
+else
+    echo 'OK: '$found''
+    exit $ok_exit
+fi
